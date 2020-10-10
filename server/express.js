b310df22717b035a0a6f6d684a701cb89dbe5f08
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-10 14:41:37 -0500

    auth error handling for express-jwt

--- server/express.js
+++ server/express.js
@@ -25,4 +25,13 @@ app.get('/', (req, res) => {
   res.status(200).send(Template())
  })
 
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+  }
+})
+
 export default app
