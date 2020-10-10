bdc842adc6fbeb61da13af2133d6c894615880b5
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-10 10:40:42 -0500

    HTML template served at a root url

--- server/express.js
+++ server/express.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template'
 
 const app = express()
   /*... configure express ... */
@@ -13,5 +14,9 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+ })
 
 export default app
