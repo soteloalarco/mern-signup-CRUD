5161a73afc592ad6f584de0544afcdac9bd6a505
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-09 23:04:34 -0500

    nodemon dependencie added, server running

--- server/express.js
+++ server/express.js
@@ -7,11 +7,11 @@ import helmet from 'helmet'
 
 const app = express()
   /*... configure express ... */
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(cookieParser())
-  app.use(compress())
-  app.use(helmet())
-  app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
 
 export default app
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import config from './../config/config'
 import app from './express'
 
-
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
