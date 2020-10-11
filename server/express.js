662a42a48dac37ebfc57d2f3e0b71ce8df19fd35
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-11 12:41:15 -0500

    template updated to add a bundled script

--- server/express.js
+++ server/express.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
@@ -8,7 +9,15 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+// comment out when not in development mode
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
+// comment out when not in development mode
+devBundle.compile(app)
+
   /*... configure express ... */
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,6 +25,8 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 // mount routes
 app.use('/', userRoutes)
